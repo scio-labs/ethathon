8d179a9b0bf477c7cf58734c3abeca4838b0ea39
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-11-17 16:49:17 +0100

    frontend: refactored contract interactions

---
+++ packages/frontend/src/components/web3/GreeterContractInteractions.tsx
@@ -0,0 +1,52 @@
+import { Lock__factory } from '@ethathon/contracts/typechain-types'
+import { useDeployments } from '@shared/useDeployments'
+import { FC } from 'react'
+import toast from 'react-hot-toast'
+import tw from 'twin.macro'
+import { useSigner } from 'wagmi'
+
+const Button = tw.button`m-2 rounded-lg border border-current px-2 py-1 font-semibold text-gray-400 hover:text-white`
+
+export const GreeterContractInteractions: FC = () => {
+  const { data: signer } = useSigner()
+  const { contracts } = useDeployments()
+
+  const getOwner = async () => {
+    if (!signer || !contracts) return
+    const contract = Lock__factory.connect(contracts.Lock.address, signer)
+    try {
+      const owner = await contract.owner()
+      toast.success(owner)
+      console.log({ owner })
+    } catch (e) {
+      toast.error('Error while fetching owner. Try again…')
+      console.error(e)
+    }
+  }
+
+  const withdraw = async () => {
+    if (!signer || !contracts) return
+    const contract = Lock__factory.connect(contracts.Lock.address, signer)
+    try {
+      const tsx = await contract.withdraw({ gasLimit: 50000 })
+      const receipt = await tsx.wait()
+      toast.success('Successfully withdrawn!')
+      console.log({ receipt })
+    } catch (e: any) {
+      toast.error('Error while withdrawal. Try again…')
+      console.error(e)
+    }
+  }
+
+  if (!signer) return null
+
+  return (
+    <>
+      <div tw="mt-6 flex items-center">
+        <div tw="mr-2 text-gray-400">Lock.sol:</div>
+        <Button onClick={() => getOwner()}>Get Owner</Button>
+        <Button onClick={() => withdraw()}>Withdraw</Button>
+      </div>
+    </>
+  )
+}
--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -1,50 +1,15 @@
 import { HomeTopBar } from '@components/home/HomeTopBar'
 import { CenterBody } from '@components/layout/CenterBody'
-import { Lock__factory } from '@ethathon/contracts/typechain-types'
+import { GreeterContractInteractions } from '@components/web3/GreeterContractInteractions'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-import { useDeployments } from '@shared/useDeployments'
 import type { NextPage } from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
 import githubIcon from 'public/icons/social/github.svg'
 import vercelIcon from 'public/icons/vercel.svg'
-import toast from 'react-hot-toast'
-import tw from 'twin.macro'
-import { useSigner } from 'wagmi'
-
-const Button = tw.button`m-2 rounded-lg border border-current px-2 py-1 font-semibold text-gray-400 hover:text-white`
+import 'twin.macro'
 
 const HomePage: NextPage = () => {
-  const { data: signer } = useSigner()
-  const { contracts } = useDeployments()
-
-  const getOwner = async () => {
-    if (!signer || !contracts) return
-    const contract = Lock__factory.connect(contracts.Lock.address, signer)
-    try {
-      const owner = await contract.owner()
-      toast.success(owner)
-      console.log({ owner })
-    } catch (e) {
-      toast.error('Error while fetching owner. Try again…')
-      console.error(e)
-    }
-  }
-
-  const withdraw = async () => {
-    if (!signer || !contracts) return
-    const contract = Lock__factory.connect(contracts.Lock.address, signer)
-    try {
-      const tsx = await contract.withdraw({ gasLimit: 50000 })
-      const receipt = await tsx.wait()
-      toast.success('Successfully withdrawn!')
-      console.log({ receipt })
-    } catch (e: any) {
-      toast.error('Error while withdrawal. Try again…')
-      console.error(e)
-    }
-  }
-
   return (
     <>
       {/* Top Bar */}
@@ -53,7 +18,7 @@ const HomePage: NextPage = () => {
         <div tw="hidden sm:inline"> – Substrate-based DApp Boilerplate</div>
       </HomeTopBar>
 
-      <CenterBody>
+      <CenterBody tw="my-12">
         {/* Title */}
         <div tw="flex flex-col items-center text-center font-mono">
           <Link
@@ -83,14 +48,8 @@ const HomePage: NextPage = () => {
         {/* Rainbowkit Connect Button */}
         <ConnectButton />
 
-        {/* Lock.sol Contract Interactions */}
-        {signer && (
-          <div tw="mt-6 flex items-center">
-            <div tw="mr-2 text-gray-400">Lock.sol:</div>
-            <Button onClick={() => getOwner()}>Get Owner</Button>
-            <Button onClick={() => withdraw()}>Withdraw</Button>
-          </div>
-        )}
+        {/* Greeter.sol Contract Interactions */}
+        <GreeterContractInteractions />
       </CenterBody>
     </>
   )
